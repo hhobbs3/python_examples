b8341ce96ab48fc7543a179997b3abda8e8bd491
Harlen Hobbs <harlenhobbs3@gmail.com>
2021-09-16 06:45:25 -0400

    sorting algorithms practice

---
+++ sort_practice.py
@@ -0,0 +1,98 @@
+from random import randint
+from timeit import repeat
+
+def quicksort(array):
+    # If the input array contains fewer than two elements,
+    # then return it as the result of the function
+    if len(array) < 2:
+        return array
+
+    low, same, high = [], [], []
+
+    # Select your `pivot` element randomly
+    pivot = array[randint(0, len(array) - 1)]
+
+    for item in array:
+        # Elements that are smaller than the `pivot` go to
+        # the `low` list. Elements that are larger than
+        # `pivot` go to the `high` list. Elements that are
+        # equal to `pivot` go to the `same` list.
+        if item < pivot:
+            low.append(item)
+        elif item == pivot:
+            same.append(item)
+        elif item > pivot:
+            high.append(item)
+
+    # The final result combines the sorted `low` list
+    # with the `same` list and the sorted `high` list
+    return quicksort(low) + same + quicksort(high)
+
+
+def quickish_sort(array):
+    if len(array) <= 2:
+        return array
+
+    low, same, high = [], [], []
+    pivot = array[randint(0, len(array) - 1)]
+
+    for item in array:
+        if item < pivot:
+            low.append(item)
+        elif item == pivot:
+            same.append(item)
+        elif item > pivot:
+            high.append(item)
+    return quickish_sort(low) + same + quickish_sort(high)
+
+def insertion_sort(array):
+    # Loop from the second element of the array until
+    # the last element
+    for i in range(1, len(array)):
+        # This is the element we want to position in its
+        # correct place
+        key_item = array[i]
+
+        # Initialize the variable that will be used to
+        # find the correct position of the element referenced
+        # by `key_item`
+        j = i - 1
+
+        # Run through the list of items (the left
+        # portion of the array) and find the correct position
+        # of the element referenced by `key_item`. Do this only
+        # if `key_item` is smaller than its adjacent values.
+        while j >= 0 and array[j] > key_item:
+            # Shift the value one position to the left
+            # and reposition j to point to the next element
+            # (from right to left)
+            array[j + 1] = array[j]
+            j -= 1
+
+        # When you finish shifting the elements, you can position
+        # `key_item` in its correct location
+        array[j + 1] = key_item
+
+    return array
+
+def insertish_sort(array):
+    for i in range(1, len(array)):
+        key_item = array[i]
+        j = i - 1
+
+        while j >= 0 and array[j] > key_item:
+            array[j + 1] = array[j]
+            j -= 1
+        array[j + 1] = key_item
+    return array
+
+
+ARRAY_LENGTH = 1000
+
+if __name__ == "__main__":
+
+    array = [randint(0, 1000) for i in range(ARRAY_LENGTH)]
+    print(array)
+    print(quickish_sort(array))
+    print(insertish_sort(array))
+
